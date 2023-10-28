a44aadaa44e75d3606be4fca5642e6c852696e75
Willy Micieli <micieli@vivaldi.net>
2023-10-28 10:51:43 +0200

    clean code

--- src/find.rs
+++ src/find.rs
@@ -5,35 +5,40 @@ pub mod ness {
     pub struct Find {}
 
     impl Find {
-        pub fn edit_file(dir: &String, filename: &String) -> bool {
+        pub fn find(dir: &String, filename: &String) -> bool
+        {
             let paths = Path::new(dir.as_str()).read_dir().unwrap();
-
             for path in paths {
                 let d: &String = &path.unwrap().path().to_str().unwrap().to_string();
-                if Path::new(d).is_dir() {
-                    // sql query to save album
+                if Path::new(d.as_str()).is_dir() {
                     continue;
                 } else if d.contains(filename) {
-                    let mut c = Command::new(env!("EDITOR"));
-                    c.arg(Path::new(d.as_str()));
-                    return c.spawn().expect("failed to edit file").wait().expect("").success();
-                } else {
-                    continue;
+                    return true;
                 }
             }
-            if dir.eq(".") {
-                println!(
-                    "The filename {} has not been founded in the current directory",
-                    filename
-                );
-                false
-            } else {
-                println!(
-                    "The filename {} has not been founded in the {} directory",
-                    filename, dir
-                );
-                false
+            return false;
+        }
+
+        pub fn get_path(dir: &str, x: &str) -> String
+        {
+            let mut p = String::new();
+            p.push_str(dir);
+            p.push_str("/");
+            p.push_str(x);
+            if Path::new(p.as_str()).is_file() && Path::new(p.as_str()).is_absolute()
+            {
+                return p;
             }
+            String::new()
+        }
+
+        pub fn edit_file(dir: &String, filename: &String) -> bool {
+            if Find::find(dir, filename) {
+                let mut c = Command::new(env!("EDITOR"));
+                c.arg(Find::get_path(dir.as_str(), filename.as_str()).as_str());
+                return c.spawn().expect("failed to edit file").wait().expect("").success();
+            }
+            false
         }
     }
 }
